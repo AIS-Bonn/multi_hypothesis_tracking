fd4d740e5683dbad6033e1105048ac21232cbe39
razlaw <s6jarazl@uni-bonn.de>
2021-04-15 17:36:01 +0200

    added node to track humans

---
+++ include/multi_hypothesis_tracking/multi_human_tracking_node.h
@@ -0,0 +1,139 @@
+/** @file
+ *
+ * Multi human tracking node receiving human messages and providing those to the multi hypothesis tracker.
+ *
+ * @author Jan Razlaw
+ */
+
+#ifndef __MULTI_HUMAN_TRACKING_NODE_H__
+#define __MULTI_HUMAN_TRACKING_NODE_H__
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/PoseArray.h>
+
+#include <person_msgs/PersonCovList.h>
+
+#include <multi_hypothesis_tracking/multi_hypothesis_tracker.h>
+#include <multi_hypothesis_tracking/mot_publisher.h>
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+
+namespace MultiHypothesisTracker
+{
+
+typedef person_msgs::PersonCovList HumanMsg;
+
+/**
+ * @brief Ros node to track multiple hypotheses simultaneously.
+ */
+class Tracker
+{
+public:
+  /** @brief Constructor. */
+  Tracker();
+  /** @brief Destructor. */
+  ~Tracker(){ m_time_file.close(); };
+
+  /**
+   * @brief Publishes the hypotheses in several versions.
+   */
+  void publish(const ros::Time& stamp);
+
+  /**
+   * @brief Callback function for HumanMsg messages.
+   *
+   * Converts messages to measurements.
+   * Transforms measurements to #m_world_frame and passes those to the tracking algorithm.
+   *
+   * @param [in] msg    detections.
+   */
+  void detectionCallback(const HumanMsg::ConstPtr& msg);
+
+  /**
+   * @brief Transforms measurements to the target_frame.
+   *
+   * @param[in,out] measurements   measurements.
+   * @param[in]     header         header - in case there are no measurements we still want to continue to tell the tracker exactly that.
+   * @param[in]     target_frame   frame the measurements are transformed to.
+   *
+   * @return false if at least one measurement couldn't be transformed, true otherwise
+   */
+  bool transformToFrame(std::vector <Measurement>& measurements,
+                        const std_msgs::Header& header,
+                        const std::string& target_frame);
+
+  /**
+   * @brief Converts the human detections into the internal format.
+   *
+   * @param[in]     msg             detections.
+   * @param[out]    measurements    detections in tracker format.
+   */
+  void convert(const HumanMsg::ConstPtr& msg,
+               std::vector <Measurement>& measurements);
+
+  /**
+   * @brief Performs one prediction and correction step for every hypothesis.
+   *
+   * Invokes prediction step for every hypothesis.
+   * Passes measurements to multi hypothesis tracker for correction step.
+   * Filters out weak hypotheses.
+   *
+   * @param measurements    new detections.
+   */
+  void processMeasurements(const std::vector <Measurement>& measurements);
+
+  /** @brief Getter for hypotheses vector. */
+  const std::vector <std::shared_ptr<Hypothesis>>& getHypotheses();
+
+  /** @brief Getter for hypotheses that are about to get deleted. */
+  std::queue <Hypothesis>& getDeletedHypotheses();
+
+private:
+  /** @brief Subscribes to detections. */
+  ros::Subscriber m_human_detection_subscriber;
+  /** @brief Publishes results. */
+  MOTPublisher m_mot_publisher;
+
+  /** @brief Provides transforms to world frame. */
+  std::shared_ptr <tf::TransformListener> m_transform_listener;
+
+  /** @brief The functionality. */
+  MultiHypothesisTracker m_multi_hypothesis_tracker;
+
+
+  //Params
+  /** @brief Fixed frame the detections and tracks are in. */
+  std::string m_world_frame;
+  /** @brief Hypotheses are merged if their distance is below this parameter. */
+  double m_merge_distance;
+  /** @brief Hypotheses are deleted if their covariance is above this parameter. */
+  float m_max_covariance;
+  /** @brief If true, the likelihood of the measurements given the hypotheses' states is computed. */
+  bool m_compute_likelihood;
+
+  /** @brief Time when the last prediction was performed. */
+  double m_last_prediction_time;
+
+  bool m_measure_time;
+  std::chrono::microseconds m_summed_time_for_callbacks;
+  int m_number_of_callbacks;
+  std::ofstream m_time_file;
+  bool m_got_first_detections;
+};
+
+}
+
+#endif
---
+++ src/multi_human_tracking_node.cpp
@@ -0,0 +1,218 @@
+/** @file
+ *
+ * Multi human tracking node receiving human messages and providing those to the multi hypothesis tracker.
+ *
+ * @author Jan Razlaw
+ */
+
+#include <multi_hypothesis_tracking/multi_human_tracking_node.h>
+
+namespace MultiHypothesisTracker
+{
+
+Tracker::Tracker()
+  : m_multi_hypothesis_tracker(std::make_shared<HypothesisFactory>())
+    , m_last_prediction_time(0)
+    , m_measure_time(false)
+    , m_number_of_callbacks(0)
+    , m_got_first_detections(false)
+{
+  ros::NodeHandle private_node_handle("~");
+
+  m_transform_listener = std::make_shared<tf::TransformListener>();
+
+  private_node_handle.param<std::string>("world_frame", m_world_frame, "world");
+
+  private_node_handle.param<double>("merge_close_hypotheses_distance", m_merge_distance, 0.1);
+  private_node_handle.param<float>("max_covariance", m_max_covariance, 5.f);
+
+  double max_correspondence_distance;
+  private_node_handle.param<double>("max_correspondence_distance", max_correspondence_distance, 3.75);
+  m_multi_hypothesis_tracker.setMaxCorrespondenceDistance(max_correspondence_distance);
+
+  double kalman_covariance_per_second;
+  private_node_handle.param<double>("kalman_covariance_per_second", kalman_covariance_per_second, 0.5);
+  m_multi_hypothesis_tracker.setKalmanCovariancePerSecond(kalman_covariance_per_second);
+
+  private_node_handle.param<bool>("compute_likelihood", m_compute_likelihood, false);
+  m_multi_hypothesis_tracker.setComputeLikelihood(m_compute_likelihood);
+
+  private_node_handle.getParam("measure_time", m_measure_time);
+  if(m_measure_time)
+  {
+    std::string path_to_results_file = "/tmp/times_multi_hypothesis_tracking";
+    m_time_file.open(path_to_results_file);
+  }
+  m_summed_time_for_callbacks = std::chrono::microseconds::zero();
+
+  std::string input_topic;
+  private_node_handle.param<std::string>("input_topic", input_topic, "/object_poses");
+  m_human_detection_subscriber = private_node_handle.subscribe<HumanMsg>(input_topic, 1,
+                                                                         &Tracker::detectionCallback,
+                                                                         this);
+}
+
+void Tracker::publish(const ros::Time& stamp)
+{
+  m_mot_publisher.publishAll(getHypotheses(), stamp);
+  m_mot_publisher.publishDeletedHypotheses(getDeletedHypotheses());
+  if(m_compute_likelihood)
+    m_mot_publisher.publishLikelihood(m_multi_hypothesis_tracker.getAverageLikelihood());
+}
+
+void Tracker::detectionCallback(const HumanMsg::ConstPtr& msg)
+{
+  ROS_DEBUG_STREAM("Tracker::detectionCallback.");
+
+  auto callback_start_time = std::chrono::high_resolution_clock::now();
+
+  std::vector <Measurement> measurements;
+  convert(msg, measurements);
+
+  if(!transformToFrame(measurements, msg->header, m_world_frame))
+    return;
+
+  m_mot_publisher.publishMeasurementPositions(measurements, msg->header.stamp);
+  m_mot_publisher.publishMeasurementsCovariances(measurements, msg->header.stamp);
+
+  processMeasurements(measurements);
+
+  if(!m_got_first_detections && !msg->persons.empty())
+    m_got_first_detections = true;
+
+  if(m_measure_time && m_got_first_detections)
+  {
+    std::chrono::microseconds time_for_one_callback = std::chrono::duration_cast<std::chrono::microseconds>(
+      std::chrono::high_resolution_clock::now() - callback_start_time);
+    ROS_DEBUG_STREAM("Time for tracking for one cloud: " << time_for_one_callback.count() << " microseconds.");
+    m_time_file << (double)time_for_one_callback.count() / 1000.0 << std::endl;
+    m_summed_time_for_callbacks += time_for_one_callback;
+    m_number_of_callbacks++;
+    ROS_DEBUG_STREAM(
+      "Mean time for tracking for one cloud: " << m_summed_time_for_callbacks.count() / m_number_of_callbacks
+                                               << " microseconds.");
+  }
+
+  publish(msg->header.stamp);
+}
+
+void Tracker::convert(const HumanMsg::ConstPtr& msg,
+                      std::vector <Measurement>& measurements)
+{
+  Measurement measurement;
+  measurement.frame = msg->header.frame_id;
+  measurement.time = msg->header.stamp.toSec();
+
+  float score_threshold = 0.1f;
+  for(const auto& person_detection : msg->persons)
+  {
+    if((int)person_detection.keypoints.size() != 21)
+      ROS_INFO_STREAM("Person detection has " << (int)person_detection.keypoints.size() << " keypoints, but should have 21.");
+
+    if(person_detection.keypoints[8].score < score_threshold)
+    {
+      ROS_INFO_STREAM("Mid hip score is " << person_detection.keypoints[8].score << ", which is below the threshold of 0.1");
+      continue;
+    }
+    
+    measurement.pos(0) = static_cast<float>(person_detection.keypoints[8].joint.x);
+    measurement.pos(1) = static_cast<float>(person_detection.keypoints[8].joint.y);
+    measurement.pos(2) = static_cast<float>(person_detection.keypoints[8].joint.z);
+    
+    float measurement_std = 0.03f;
+    measurement.cov.setIdentity();
+    measurement.cov(0, 0) = measurement_std * measurement_std;
+    measurement.cov(1, 1) = measurement_std * measurement_std;
+    measurement.cov(2, 2) = measurement_std * measurement_std;
+
+    measurement.class_a_detection = true;
+
+    measurements.push_back(measurement);
+  }
+}
+
+bool Tracker::transformToFrame(std::vector <Measurement>& measurements,
+                               const std_msgs::Header& header,
+                               const std::string& target_frame)
+{
+  if(target_frame == header.frame_id)
+    return true;
+
+  if(!m_transform_listener->waitForTransform(target_frame, header.frame_id, header.stamp, ros::Duration(1.0)))
+  {
+    ROS_ERROR_STREAM("Could not wait for transform at time " << header.stamp);
+    return false;
+  }
+
+  tf::StampedTransform transform;
+  try
+  {
+    m_transform_listener->lookupTransform(target_frame, header.frame_id, header.stamp, transform);
+  }
+  catch(tf::TransformException& ex)
+  {
+    ROS_ERROR("Received an exception trying to transform a point from \"%s\" to \"%s\"", header.frame_id.c_str(),
+              target_frame.c_str());
+    return false;
+  }
+
+  for(auto& measurement : measurements)
+  {
+    Eigen::Affine3d transform_eigen;
+    tf::transformTFToEigen(transform, transform_eigen);
+    Eigen::Affine3f transform_eigenf = transform_eigen.cast<float>();
+
+    measurement.pos = transform_eigenf * measurement.pos;
+    measurement.frame = target_frame;
+  }
+
+  return true;
+}
+
+void Tracker::processMeasurements(const std::vector <Measurement>& measurements)
+{
+  if(measurements.empty())
+    return;
+
+  // Prediction step of kalman filter for all hypotheses
+  if(m_last_prediction_time > 0)
+    m_multi_hypothesis_tracker.predict(measurements.at(0).time - m_last_prediction_time);
+
+  m_last_prediction_time = measurements.at(0).time;
+
+  // Correction step of kalman filter for all hypotheses
+  m_multi_hypothesis_tracker.correct(measurements);
+
+  // Filter out weak hypotheses
+  m_multi_hypothesis_tracker.clearDeletedHypotheses();
+  m_multi_hypothesis_tracker.deleteSpuriousHypotheses(m_max_covariance);
+  m_multi_hypothesis_tracker.mergeCloseHypotheses(m_merge_distance);
+}
+
+const std::vector <std::shared_ptr<Hypothesis>>& Tracker::getHypotheses()
+{
+  return m_multi_hypothesis_tracker.getHypotheses();
+}
+
+std::queue <Hypothesis>& Tracker::getDeletedHypotheses()
+{
+  return m_multi_hypothesis_tracker.getDeletedHypotheses();
+}
+
+}
+
+
+int main(int argc,
+         char** argv)
+{
+  ros::init(argc, argv, "multi_human_tracking");
+
+  if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info))
+    ros::console::notifyLoggerLevelsChanged();
+
+  MultiHypothesisTracker::Tracker tracker;
+
+  ros::spin();
+
+  return 0;
+}
