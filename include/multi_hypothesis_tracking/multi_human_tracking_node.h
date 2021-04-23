3d957fa39c2fa9aa5d2de1995b50e6999d64cd54
razlaw <s6jarazl@uni-bonn.de>
2021-04-23 16:49:24 +0200

    removed unused includes

--- include/multi_hypothesis_tracking/multi_human_tracking_node.h
+++ include/multi_hypothesis_tracking/multi_human_tracking_node.h
@@ -8,10 +8,6 @@
 #ifndef __MULTI_HUMAN_TRACKING_NODE_H__
 #define __MULTI_HUMAN_TRACKING_NODE_H__
 
-#include <chrono>
-#include <fstream>
-#include <iostream>
-
 #include <person_msgs/PersonCovList.h>
 
 #include <multi_hypothesis_tracking/definitions.h>
